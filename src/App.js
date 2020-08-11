6016171277e60375039c075086b8f1bf44fae57a
johnvergara01 <john.vergara01@gmail.com>
2020-08-10 17:46:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfGuests: 2,
+      subtotal: 0.0,
+      tipPercent: 0.0,
+      splitEvenly: true,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.name === 'splitEvenly' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  calculateGrandTotal = () => ((parseFloat(this.state.subtotal) + parseFloat(this.state.subtotal) * parseFloat(this.state.tipPercent) / 100) / parseFloat(this.state.numberOfGuests)).toFixed(2)
+
+  render() {
+    return (
+      <form>
+        <label>
+          Number of guests:
+          <input 
+            min="1"
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <br />
+        <label>
+          Subtotal: $
+          <input 
+            min="0"
+            name="subtotal"
+            type="number"
+            value={this.state.subtotal}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <br />
+        <label>
+          Tip Percent:
+          <input 
+            min="0"
+            name="tipPercent"
+            type="number"
+            value={this.state.tipPercent}
+            onChange={this.handleInputChange} />
+        </label>
+        %
+        <br />
+        <div>
+          <br />
+          Grand Total: $
+          {this.calculateGrandTotal()} per person
+      </div>
+      </form>
+    );
+  }
 }
 
 export default App;
